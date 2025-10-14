72d8375621809af9ef6f8c10ace74581359dd105
zxcdebil MB <mishany1105@gmail.com>
2025-10-14 07:16:42 +0300

    MVP v0.1

--- src/backend/adapters/db/migrations/env.py
+++ src/backend/adapters/db/migrations/env.py
@@ -1,14 +1,12 @@
 import asyncio
 from logging.config import fileConfig
 
+from adapters.db.models import Base
+from alembic import context
+from app.core.settings import config
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-
-from alembic import context
-
-from adapters.db.models import Base
-from app.core.settings import config
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
